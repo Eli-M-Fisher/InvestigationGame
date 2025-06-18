56e08de48e3360bc570f750c6fe836847a9da276
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-06-18 10:58:49 +0300

    add a to string method

--- obj/Debug/net9.0/InvestigationGame.AssemblyInfo.cs
+++ obj/Debug/net9.0/InvestigationGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvestigationGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a98da0de91def891f33eb0fa891960f46e4fd493")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd3fbb357bc3662ede985d7f5adac7e2c0cec4be")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvestigationGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvestigationGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
